17594724f4767a418c43f13925a4273a7e0b45b1
Danny Yoo <dyoo@hashcollision.org>
2013-01-25 14:35:09 -0700

    newline

--- ffi/tutorial/examples/hello/pre-installer.rkt
+++ ffi/tutorial/examples/hello/pre-installer.rkt
@@ -5,9 +5,10 @@
 (require ffi/tutorial/utils)
 
 (define pre-installer
-  (make-pre-installer "hello" #:sources (list "src/hello.c")))
+  (make-pre-installer "hello" 
+                      #:sources (list "src/hello.c")))
 
-;; We can try building the libray by hand by just calling the
+;; We can try building the library by hand by just calling the
 ;; pre-installer, but be sure to first cd into this source's directory
 ;; before doing so.
 (module+ main
