88404d93f4a7b8395105a3f9239b5cb469dfa89b
Danny Yoo <dyoo@hashcollision.org>
2013-02-19 15:08:21 -0700

    trying to figure out what's going on.

--- ffi/tutorial/examples/hello-2/hello.rkt
+++ ffi/tutorial/examples/hello-2/hello.rkt
@@ -12,15 +12,16 @@
 (define the-lib (ffi-lib hello-library-path))
 (define system-lib (ffi-lib #f))
 
-(define scheme_printf
-  (get-ffi-obj "scheme_printf" system-lib
-               (_fun _bytes _int _int _racket -> void)))
+(define scheme_printf_utf8
+  (get-ffi-obj "scheme_printf_utf8" system-lib
+               (_fun _bytes _int _int (_ptr i _racket) -> _void)))
+
 
 (define _say-hello (get-ffi-obj "sayHello" the-lib
-                                (_fun (_fun _bytes _int _int _racket -> _void)
+                                (_fun (_fun _bytes _int _int (_ptr i _racket) -> _void)
                                       _racket 
                                       -> 
                                       _void)))
 
 (define (say-hello n)
-  (_say-hello scheme_printf n))
+  (_say-hello scheme_printf_utf8 n))
--- ffi/tutorial/examples/hello-2/src/hello.c
+++ ffi/tutorial/examples/hello-2/src/hello.c
@@ -4,5 +4,6 @@ void sayHello(void (*scheme_printf_utf8)
               (char* format, int flen, int argc, void** argv),
               void *name) {
   char* msg = "~s says: hello!";
-  scheme_printf_utf8(msg, strlen(msg), 1, name);
+  void **args = { name };
+  scheme_printf_utf8(msg, strlen(msg), 1, args);
 }
