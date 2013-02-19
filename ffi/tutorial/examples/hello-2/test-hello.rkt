b83b945b020090a214055be8d6aa77673f5107db
Danny Yoo <dyoo@hashcollision.org>
2013-02-19 15:12:42 -0700

    Trying to get printout working.

--- ffi/tutorial/examples/hello-2/test-hello.rkt
+++ ffi/tutorial/examples/hello-2/test-hello.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require "hello.rkt")
 
-
 ;; Now we can just call say-hello:
-(printf "before\n")(say-hello "Danny")(printf "\nafter\n")
+(printf "before\n")
+(say-hello "Danny")
+(printf "\nafter\n")
