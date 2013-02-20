a6a3865b82db926c5f36d657a4a40e60e94b0a45
Danny Yoo <dyoo@hashcollision.org>
2013-02-20 11:38:43 -0700

    starting to refactor.

--- ffi/tutorial/examples/hello-2/hello.rkt
+++ ffi/tutorial/examples/hello-2/hello.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 (require ffi/unsafe
+         ffi/unsafe/cvector
          racket/runtime-path
          (for-syntax racket/base))
 
@@ -12,13 +13,21 @@
 (define the-lib (ffi-lib hello-library-path))
 (define system-lib (ffi-lib #f))
 
+;; Expose the Scheme level printf function for the C code to use.
+;; The type of the printf function is:
+(define scheme_printf_utf8_t
+  (_fun (format-str : _bytes)
+        (format-str-len : _int)
+        (argc : _int)
+        (argv : (_ptr i _racket))
+        -> _void))
+
 (define scheme_printf_utf8
-  (get-ffi-obj "scheme_printf_utf8" system-lib
-               (_fun _bytes _int _int (_ptr i _racket) -> _void)))
+  (get-ffi-obj "scheme_printf_utf8" system-lib scheme_printf_utf8_t))
 
 
 (define _say-hello (get-ffi-obj "sayHello" the-lib
-                                (_fun (_fun _bytes _int _int (_ptr i _racket) -> _void)
+                                (_fun scheme_printf_utf8_t
                                       _racket 
                                       -> 
                                       _void)))
