cc1d81effd7274a9869220fb1a978c2dcc3ed4c3
Danny Yoo <dyoo@hashcollision.org>
2013-02-20 11:54:14 -0700

    getting printf to work.

--- ffi/tutorial/examples/hello-2/src/hello.c
+++ ffi/tutorial/examples/hello-2/src/hello.c
@@ -3,6 +3,6 @@
 void sayHello(void (*scheme_printf_utf8)
               (char* format, int flen, int argc, void** argv),
               void *name) {
-  char* msg = "hello ~a!  This is a test";
+  char* msg = "hello ~a";
   scheme_printf_utf8(msg, strlen(msg), 1, &name);
 }
