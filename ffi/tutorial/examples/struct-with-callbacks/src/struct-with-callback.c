ef2795be0f44314347403c952b98ecb0149c6085
Danny Yoo <dyoo@hashcollision.org>
2013-02-25 13:02:55 -0700

    skeleton

---
+++ ffi/tutorial/examples/struct-with-callbacks/src/struct-with-callback.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct foo {
+  int a;
+  char b[42];
+};
+
+
+void printFoo(struct foo f) {
+  printf("<struct:foo %d %s>", f.a, f.b);
+  fflush(stdout);
+}
---
+++ ffi/tutorial/examples/struct-with-callbacks/struct-with-callback.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+(require ffi/unsafe
+         racket/runtime-path
+         (for-syntax racket/base))
+
+(provide (except-out (all-defined-out) the-lib the-library-path))
+
+(define-runtime-path the-library-path
+  (build-path "lib" "struct-with-array"))
+
+#|
+struct foo {
+  int a;
+  char b[42];
+};
+|#
+
+
+;; Creates a sized bytes type of length n.
+(define (_bytes/len n)
+  (make-ctype (make-array-type _byte n)
+
+              ;; ->c
+              (lambda (v)
+                (unless (and (bytes? v) (= (bytes-length v) n))
+                  (raise-argument-error '_chars/bytes 
+                                        (format "bytes of length ~a" n)
+                                        v))
+                v)
+
+              ;; ->racket
+              (lambda (v)
+                (make-sized-byte-string v n))))
+
+
+(define-cstruct _foo ([a _int]
+                      [b (_bytes/len 42)]))
+
+(define the-lib (ffi-lib the-library-path))
+
+(define print-foo 
+  (get-ffi-obj "printFoo" the-lib
+               (_fun _foo -> _void)))
